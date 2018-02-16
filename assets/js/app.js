7aa957f6a8c1b6bd8c42c78c432bcc453b30b791
bimiriya <poyarzunaspee@gmail.com>
2018-02-16 07:59:57 -0300

    merged

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@ const pokeapi = fetch("http://pokeapi.co/api/v2/pokemon/");
 pokeapi
   .then(response => response.json())
   .then(data => {
-    // console.log(data);
     $.each(data.results, function (pkm, i) {
       const sprites = fetch("http://pokeapi.co/api/v2/pokemon/" + i.name);
       sprites.then(response => response.json()).then(data => {
@@ -18,11 +17,6 @@ pokeapi
   });
   window.onload = function() {
 $("#results").on("click", ".card", function () {
-  // console.log(
-  //   $(this)
-  //     .find("p")
-  //     .text()
-  // );
   const sprites = fetch(
     `http://pokeapi.co/api/v2/pokemon/${$(this)
       .find("p")
@@ -43,51 +37,6 @@ $("#results").on("click", ".card", function () {
       defense.setAttribute("value", data.stats[3].base_stat);
       attack.setAttribute("value", data.stats[4].base_stat);
       hp.setAttribute("value", data.stats[5].base_stat);
-    //   $('#profile').html(`<div id="profile-box">
-    //       <div>
-    //         <h2>${data.name.toUpperCase()}</h2>
-    //       </div>
-    //       <div>
-    //         <table class="table" id="table">
-    //           <tr>
-    //               <td>Height:</td>
-    //               <td>${data.height/10}m</td>
-    //           </tr>
-    //           <tr>
-    //               <td>Weight:</td>
-    //               <td>${data.weight/10}kg</td>
-    //           </tr>
-    //           <tr>
-    //               <td>Abilities:</td>
-    //               <td>${i.ability.name}</td>
-    //           </tr>
-    //           <tr>
-    //               <td>Speed:</td>
-    //               <td><progress value="${data.stats[0].base_stat}" max="100"></progress></td>
-    //           </tr>
-    //           <tr>
-    //               <td>Special-defense</td>
-    //               <td><progress value="${data.stats[1].base_stat}" max="100"></progress></td>
-    //           </tr>
-    //           <tr>
-    //               <td>Special-attack</td>
-    //               <td><progress value="${data.stats[2].base_stat}" max="100"></progress></td>
-    //           </tr>
-    //           <tr>
-    //               <td>Defense</td>
-    //               <td><progress value="${data.stats[3].base_stat}" max="100"></progress></td>
-    //           </tr>
-    //           <tr>
-    //               <td>Attack:</td>
-    //               <td><progress value="${data.stats[4].base_stat}" max="100"></progress></td>
-    //           </tr>
-    //           <tr>
-    //               <td>Hp</td>
-    //               <td><progress value="${data.stats[5].base_stat}" max="100"></progress></td>
-    //           </tr>
-    //       </table>
-    //       </div>
-    // </div>`);
     });
   });
 });
